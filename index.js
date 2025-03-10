7244c5e155116a90b9542192e2cf30f47c86f2a5
MirotadzeLuka <167008317+LuXClypse@users.noreply.github.com>
2025-03-10 20:13:23 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,190 @@
+import express from "express";
+import cors from "cors";
+import { connectToDatabase } from "./services/database.js";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+    async function run() {
+        try {
+            const database = await connectToDatabase();
+        } catch (error) {
+            console.error('Error handling request:', error);
+            res.status(500).json({ error: 'Internal server error' });
+            return;
+        }
+    }
+
+    await run();
+    res.json({ status: "mongo is running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/api/users', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const users = database.collection('users');
+//     const cursor = users.find();
+//     const result = await cursor.toArray();
+//     res.json(result);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.post('/api/users', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const users = database.collection('users');
+//     const doc = {
+//       name: req.body.name,
+//       email: req.body.email,
+//       password: req.body.password,
+//     };
+//     const result = await users.insertOne(doc);
+//     res.json(result);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.patch('/api/users/changeUser', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const users = database.collection('users');
+//     const filter = { email: req.body.email };
+//     const updateDoc = {
+//       $set: { name: req.body.name },
+//     };
+//     const result = await users.updateOne(filter, updateDoc);
+//     res.status(result.modifiedCount === 0 ? 401 : 200).json({ statusCode: result.modifiedCount === 0 ? 401 : 200, result });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+//
+// app.delete('/api/users/deleteUser', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const users = database.collection('users');
+//
+//     const query = { email: req.body.email, password: req.body.password };
+//     const result = await users.deleteOne(query);
+//
+//     if (result.deletedCount === 0) {
+//       return res.status(404).json({ statusCode: 404, message: "User not found" });
+//     }
+//     res.status(result.modifiedCount === 0 ? 401 : 200).json({ statusCode: result.modifiedCount === 0 ? 401 : 200, result });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.get('/api/theaters', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const theaters = database.collection('theaters');
+//     const cursor = theaters.find();
+//     const result = await cursor.toArray();
+//     res.json(result);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.patch('/api/theaters/changeTheater', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const theaters = database.collection('theaters');
+//     const filter = { name: req.body.name };
+//     const updateDoc = {
+//       $set: { location: req.body.location },
+//     };
+//     const result = await theaters.updateOne(filter, updateDoc);
+//     res.status(result.modifiedCount === 0 ? 401 : 200).json({ statusCode: result.modifiedCount === 0 ? 401 : 200, result });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.get('/api/comments', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const comments = database.collection('comments');
+//     const cursor = comments.find();
+//     const result = await cursor.toArray();
+//     res.json(result);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.post('/api/comments', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const comments = database.collection('comments');
+//     const date = new Date();
+//     const movieObjectId = new ObjectId(req.body.movie_id);
+//     const doc = {
+//       name: req.body.name,
+//       email: req.body.email,
+//       movie_id: movieObjectId,
+//       text: req.body.text,
+//       date,
+//     };
+//     const result = await comments.insertOne(doc);
+//     res.json(result);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+//
+// app.patch('/api/comments/changeComment', async (req, res) => {
+//   try {
+//     const database = await connectToDatabase();
+//     const comments = database.collection('comments');
+//     const filter = { email: req.body.email, movie_id: new ObjectId(req.body.movie_id) };
+//     const updateDoc = {
+//       $set: { text: req.body.text },
+//     };
+//     const result = await comments.updateOne(filter, updateDoc);
+//     res.status(result.modifiedCount === 0 ? 401 : 200).json({ statusCode: result.modifiedCount === 0 ? 401 : 200, result });
+//   } catch (error) {
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
+
---
+++ services/database.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+const uri = 'mongodb+srv://luka_mirotadze:9VTq2BaZXOW4pj5X@cluster0.8p2gd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to the database');
+        return client.db('cyber_DB');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        throw new Error('Database connection failed');
+    }
+}
+
+async function closeConnection() {
+    try {
+        await client.close();
+        console.log('Database connection closed');
+    } catch (error) {
+        console.error('Error closing the database connection:', error);
+    }
+}
+
+export { connectToDatabase, client, closeConnection };
